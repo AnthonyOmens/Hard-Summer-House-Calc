a215831880b1f4f3edb1462260bbb56d11645256
MonkiKong <aomens@sbcglobal.net>
2022-07-12 02:59:33 +0000

    added junk code dot py

---
+++ junkCode.py
@@ -0,0 +1,72 @@
+# JUNK CODE BELOW
+# not ready to delete just yet...
+
+
+# 2 people paying 280 ROOM
+# 8 people paying 260 BED
+# total = 2640
+
+# Rooms = 2
+# Beds = 8
+
+# HousePeopleTotal = 10
+# HouseAmountTotal = (280*Rooms) + (260*Beds)
+# print(f"House Amount Total = " + str(HouseAmountTotal))
+
+# #Fern and Kuz paying 21% of the place
+# #Everyone else paying 79%
+
+# Total = 2640
+
+# RoomsTotal = Total/Rooms
+# BedsTotal = Total/Beds
+
+# print("Room Total = " + str(RoomsTotal))
+# print("Bed Total = " + str(BedsTotal))
+
+
+
+
+
+# print("--------------------")
+
+
+# totalCost = 1000
+# Rooms = 2
+# Beds = 8
+
+# AdditinalPpl = 2
+
+# PeopleTotal = Beds + Rooms
+
+# RoomPplPercent = Rooms/PeopleTotal
+# BedPplPercent = Beds/PeopleTotal
+
+# print("Room People Percent = "+ str(RoomPplPercent))
+# print("Bed People Percent = "+ str(BedPplPercent))
+
+# BedPplTotalAmm = BedPplPercent * totalCost
+# print("Bed People Total cost = " + str(BedPplTotalAmm))
+
+# RoomPplTotalAmm = RoomPplPercent * totalCost
+# print("Room People Total cost = " + str(RoomPplTotalAmm))
+
+# print("Fern and Kuz each pay " + str(RoomPplTotalAmm/Rooms))
+# print("Everyone else each pay str " + str(BedPplTotalAmm/Beds))
+
+#testing
+
+
+
+
+
+
+# print("-------------")
+
+# FnK = .25
+# EveryoneElse = .75
+
+# FnkCost = FnK * totalCost
+# print("Fern n Kuz total pay is " + str(FnkCost))
+# EveryoneCost = EveryoneElse * totalCost
+# print("Everyone else total pay is " + str(EveryoneCost))
